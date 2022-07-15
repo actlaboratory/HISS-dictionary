e28997f645be5bcfa6983c5d07f9336235fd7f2d
Yukio Nozawa <personal@nyanchangames.com>
2022-07-15 23:12:14 +0900

    Add csv alphabet fixer

---
+++ tools/fixer.py
@@ -0,0 +1,13 @@
+with open("main.csv", "r", encoding="UTF-8") as f:
+    lines = f.readlines()
+
+lines = [l.split(",") for l in lines]
+
+for i in range(len(lines)):
+    lines[i][0] = lines[i][0].translate(str.maketrans(
+        {chr(0xFF01 + j): chr(0x21 + j) for j in range(94)}))
+    lines[i][0] = lines[i][0].translate(str.maketrans(
+        {chr(0x41 + j): chr(0x61 + j) for j in range(26)}))
+
+with open("main.csv", "w", encoding="UTF-8") as f:
+    f.writelines([",".join(l) for l in lines])
