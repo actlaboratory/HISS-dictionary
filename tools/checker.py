9bf73b37e5c49164e7254486c161540a7dca9733
Yukio Nozawa <personal@nyanchangames.com>
2022-07-15 23:40:18 +0900

    Fix offset

--- tools/checker.py
+++ tools/checker.py
@@ -23,13 +23,13 @@ for i in range(len(lines)):
         continue
     ls = lines[i].split(",")
     if len(ls) != 3:
-        errors.append((i, "要素の数が合いません。"))
+        errors.append((i+1, "要素の数が合いません。"))
         continue
     if re.search(pattern, ls[0]):
-        errors.append((i, "単語にアルファベットを使う場合は、半角の小文字にしてください。"))
+        errors.append((i+1, "単語にアルファベットを使う場合は、半角の小文字にしてください。"))
         continue
     if ls[0] in words:
-        errors.append((i, "同じ単語がすでに登録されています。"))
+        errors.append((i+1, "同じ単語がすでに登録されています。"))
     words[ls[0]] = True
 
 
