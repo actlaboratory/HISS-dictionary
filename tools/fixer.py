8be51f6d39564a7d66737cec19cb490b1dcc38a0
Yukio Nozawa <personal@nyanchangames.com>
2022-09-10 11:16:19 +0900

    Ignore lines starting with a number sign

--- tools/fixer.py
+++ tools/fixer.py
@@ -4,6 +4,8 @@ with open("main.csv", "r", encoding="UTF-8") as f:
 lines = [l.split(",") for l in lines]
 
 for i in range(len(lines)):
+    if lines[i][0][0] == "#":
+        continue
     lines[i][0] = lines[i][0].translate(str.maketrans(
         {chr(0xFF01 + j): chr(0x21 + j) for j in range(94)}))
     lines[i][0] = lines[i][0].translate(str.maketrans(
